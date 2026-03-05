9967260599c7b45b90850f91e8963b1d44666095
YJU <aya21110@naver.com>
2026-03-05 12:08:17 +0900

    frist commit

---
+++ src/main/java/me/sylee/springdeveloper/HelloWorldController.java
@@ -0,0 +1,27 @@
+package me.sylee.springdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    // http://localhost:8080/student?fristName=김&lastName=동
+    @GetMapping("/student")
+    public Student getStudent(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+    // htttp://localhost:8080/student/김/길동
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student getStudent2(@PathVariable("firstName")  String firstName, @PathVariable("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+}
